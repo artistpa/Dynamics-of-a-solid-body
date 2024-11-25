d4c47324211879fb55d76658893b4f1a3997935f
artistpa <lerner.pa@phystech.edu>
2024-11-25 18:11:51 +0300

    added quaternion and three equations

--- Dynamics-of-a-solid-body.py
+++ Dynamics-of-a-solid-body.py
@@ -21,7 +21,24 @@ class Solid_body:
     def __get_S_inv__(self):
         return sp.linalg.inv(self.__get_main_axes__())
 
-#definition of the external force
+    def __set_omega__(self, omega):
+        self.omega = omega
+
+#definition of the quaternion
+class Quaternion:
+    def __init__(self, angle, axis):
+        self.l0 = np.cos(angle/2)
+        self.l1 = axis[0] * np.sin(angle/2)
+        self.l2 = axis[1] * np.sin(angle/2)
+        self.l3 = axis[2] * np.sin(angle/2)
+    def __sum__(self, Quat):
+        q = Quaternion(0,[0, 0, 0])
+        q.l0 = self.l0 + Quat.l0
+        q.l1 = self.l1 + Quat.l1
+        q.l2 = self.l2 + Quat.l2
+        q.l3 = self.l3 + Quat.l3
+
+#definition of the external forces
 class External_force:
     def __init__(self, F, Rc):
         self.F = F
@@ -36,8 +53,25 @@ I = np.array([[1., 1., 1.],
 omega0 = np.array([0., 2., 1.], dtype= np.float64)
 theta = np.pi / 6
 a = Solid_body(I, omega0, theta)
-first_eq = sp.Eq()
-print(a.__get_main_I__())
-print(a.__get_S_inv__() @ a.I @ a.__get_main_axes__())
-print(a.__get_main_axes__())
-
+#print(a.__get_main_I__())
+#print(a.__get_S_inv__() @ a.I @ a.__get_main_axes__())
+#print(a.__get_main_axes__())
+A = a.__get_main_I__()[0][0]
+B = a.__get_main_I__()[1][1]
+C = a.__get_main_I__()[2][2]
+#print(A, B, C)
+angle0 = 0
+axis0 = np.array([1, 0, 0])
+L0 = Quaternion(angle0, axis0)
+F = np.array([0, 10, 0])
+Rc = np.array([1,1,1])
+Fex = External_force(F, Rc)
+Mex = Fex.__get_M__()
+omega = np.array([0,0,1])
+a.__set_omega__(omega)
+p = a.omega[0]
+q = a.omega[1]
+r = a.omega[2]
+dpdt =(Mex[0] + (B - C) * q * r) / A
+dqdt =(Mex[1] + (C - A) * p * r) / B
+drdt =(Mex[2] + (A - B) * q * p) / C

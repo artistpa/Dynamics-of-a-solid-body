2bae5cc7e5784d5b046ca90cc21081dbc700ff31
artistpa <lerner.pa@phystech.edu>
2024-12-10 14:12:21 +0300

    added 3d visualisation

--- SolidSolver.py
+++ SolidSolver.py
@@ -2,8 +2,10 @@ import sympy as sm
 import scipy as sp
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.animation as animation
 
-#definition of the quaternion
+
+# definition of the quaternion
 class Quaternion:
     def __init__(self, angle, axis):
         self.l0 = 0
@@ -12,18 +14,20 @@ class Quaternion:
         self.l[0] = axis[0] * np.sin(angle/2)
         self.l[1] = axis[1] * np.sin(angle/2)
         self.l[2] = axis[2] * np.sin(angle/2)
+
     def __sum__(self, Quat):
         q = Quaternion(0,[0, 0, 0])
         q.l0 = self.l0 + Quat.l0
         q.l = self.l + Quat.l
         return q
+
     def __mult_on_scal__(self, a):
         self.l0 *= a
         self.l *= a
 
     def __mult_on_quat__(self, Quat):
         q = Quaternion(0, [0, 0, 0])
-        q.l0 = self.l0 *  Quat.l0 - np.dot(q.l, self.l)
+        q.l0 = self.l0 * Quat.l0 - np.dot(q.l, self.l)
         q.l = self.l0 * Quat.l + Quat.l0 * self.l + np.cross(self.l, Quat.l)
         return q
 
@@ -39,7 +43,8 @@ class Quaternion:
         self.l0 = l01
         self.l = l1
 
-#creating Solid_body
+
+# creating Solid_body
 class Solid_body:
     def __init__(self, I, omega, R0):
         self.I = I
@@ -66,7 +71,18 @@ class Solid_body:
         res = (q.__mult_on_quat__(q0)).__mult_on_quat__(q1)
         return np.array([res.l[0], res.l[1], res.l[2]])
 
-#definition of the external forces
+    def get_basis(self, q):
+        res = []
+        for i in [[1, 0, 0], [0, 1, 0], [0, 0, 1]]:
+            q0 = Quaternion(np.pi, i)
+            q1 = q.__get_сonjugate_quaternion__()
+            q_res = q.__mult_on_quat__(q0).__mult_on_quat__(q1)
+            q_res.__normalization__()
+            res.append([q_res.l[0], q_res.l[1], q_res.l[2]])
+        return np.array(res)
+
+
+# definition of the external forces
 class External_force:
     def __init__(self, F, Rc):
         self.F = F
@@ -74,7 +90,8 @@ class External_force:
     def __get_M__(self):
         return np.cross(self.Rc, self.F)
 
-#derivative dU/dt
+
+# derivative dU/dt
 def g(t, U):
     dl0dt = -0.5 * (L0.l[0] * p + L0.l[1] * q + L0.l[2] * r)
     dl1dt = (L0.l0 * p + L0.l[1] * r - L0.l[2] * q)
@@ -85,12 +102,13 @@ def g(t, U):
     drdt = (Mex[2] + (A - B) * q * p) / C
     return np.array([dl0dt, dl1dt, dl2dt, dl3dt, dpdt, dqdt, drdt])
 
-#main part of the project
-I = np.array([[10., 0., 0.],
-              [0., 10., 0.],
-              [0., 0., 10.]], dtype=np.float64)
-omega0 = np.array([1, 1, 1], dtype= np.float64) #in the basis associated with the body
-R0 = np.array([0, 0, 1]) #the initial coordinates of the center of mass in the basis associated with the body
+
+# main part of the project
+I = np.array([[5., 0., 0.],
+              [0., 5., 0.],
+              [0., 0., 1.]], dtype=np.float64)
+omega0 = np.array([1, 2, 3], dtype=np.float64)  # in the basis associated with the body
+R0 = np.array([1, 1, 1])  # the initial coordinates of the center of mass in the basis associated with the body
 body = Solid_body(I, omega0, R0)
 print(body.__get_main_I__())
 print(body.__get_S_inv__() @ body.I @ body.__get_main_axes__())
@@ -102,36 +120,64 @@ p = body.omega[0]
 q = body.omega[1]
 r = body.omega[2]
 angle0 = np.pi
-axis0 = np.array([0, 0, 1])
+axis0 = np.array([1, 1, 1])
 L0 = Quaternion(angle0, axis0)
+L0.__normalization__()
 F = np.array([0, 0, 0])
-Rc = np.array([0, 0, 0])
+Rc = np.array([1, 1, 1])
 Fex = External_force(F, Rc)
 Mex = Fex.__get_M__()
-dpdt =(Mex[0] + (B - C) * q * r) / A
-dqdt =(Mex[1] + (C - A) * p * r) / B
-drdt =(Mex[2] + (A - B) * q * p) / C
+dpdt = (Mex[0] + (B - C) * q * r) / A
+dqdt = (Mex[1] + (C - A) * p * r) / B
+drdt = (Mex[2] + (A - B) * q * p) / C
 
 T = 1000
 U0 = np.array([L0.l0, L0.l[0], L0.l[1], L0.l[2], body.omega[0], body.omega[1], body.omega[2]])
 solution = sp.integrate.solve_ivp(g, (0.0, T), y0=U0, t_eval=np.linspace(0.0, T, 10000))
-#print(solution.y)
+# print(solution.y)
 
 Quats = []
 
 for i in range(len(solution.y[0])):
-    q = Quaternion(0, [0,0,0])
-    q.l0 = solution.y[0,i]
-    q.l = np.array([solution.y[1,i], solution.y[2,i], solution.y[3,i]])
+    q = Quaternion(0, [0, 0, 0])
+    q.l0 = solution.y[0, i]
+    q.l = np.array([solution.y[1, i], solution.y[2, i], solution.y[3, i]])
     q.__normalization__()
     Quats.append(q)
 
 Coordinates_cm = np.array([body.get_coordinates(quat) for quat in Quats])
+Coordinates_basis = np.array([body.get_basis(quat) for quat in Quats])
 
 plt.plot(solution.t, solution.y[6,:], '-ro')
 plt.grid()
 plt.show()
 
-plt.plot(Coordinates_cm[:,0], Coordinates_cm[:,1], '-ro')
+plt.plot(solution.t, Coordinates_cm[:,1], '-ro')
 plt.grid()
+plt.show()
+
+
+fig = plt.figure(figsize=(7, 7))
+ax = fig.add_subplot(projection="3d")
+ax.set(xlim3d=(-1.5, 1.5), xlabel='X')
+ax.set(ylim3d=(-1.5, 1.5), ylabel='Y')
+ax.set(zlim3d=(-1.5, 1.5), zlabel='Z')
+ax.set_aspect('equal', adjustable='box')
+ax.quiver(0, 0, 0, 0, 0, 2, color="blue", alpha=0.6, lw=1)
+ax.quiver(0, 0, 0, 0, 2, 0, color="green", alpha=0.6, lw=1)
+ax.quiver(0, 0, 0, 2, 0, 0, color="red", alpha=0.6, lw=1)
+
+color = ['red', 'green', 'blue']
+lines = [ax.plot([], [], [], marker="", color=color[i])[0] for i in range(3)]
+data = Coordinates_basis
+
+
+def animate(num, vecs, lines):
+    for line, vec in zip(lines, vecs[num]):
+        line.set_data_3d([0, vec[0]], [0, vec[1]], [0, vec[2]])
+    return lines
+
+
+ani = animation.FuncAnimation(fig, animate, len(data) - 1, fargs=(data, lines), interval=100)
+
 plt.show()
